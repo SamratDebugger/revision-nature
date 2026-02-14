fb486414123818e25d0c22a613b8488ddc023f26
Samrat <md721023@gmail.com>
2026-02-14 19:00:46 +0600

    environment setup

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from "react";
+import "./index.css";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router";
+import ReactDOM from "react-dom/client";
+
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+);
